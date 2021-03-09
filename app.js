f28d7f0d71841d194cbf69d6a76ee5cffabad877
maamg <maamg164@gmail.com>
2021-03-09 06:27:15 +0600

    after rewiew

--- app.js
+++ app.js
@@ -7,30 +7,33 @@ const foodSearch = foodCode => {
 }
 
 const foodDisplay = foodName => {
-    const foodName1 = foodName.meals;
-    let foodFrame = document.getElementById('foodItem');
-    let foodIngredient = document.getElementById('foodIngredient');
-    foodFrame.innerHTML = '';
-    foodName1.forEach(eachFood => {
-        const name = eachFood.strMeal;
-        // console.log(name);
-        const image = eachFood.strMealThumb;
-        const foodFrameTem = document.createElement('div');
-        foodFrameTem.innerHTML = '';
-        const info = ` <div onclick= "singleItem('${name}')" >
-        <img  src='${image}' style='width: 250px;'>
-        <h5> ${name}</h5>
-        </div>
-        `;
-        foodFrameTem.innerHTML = info;
-        foodIngredient.innerHTML ='';
-        foodFrameTem.className = 'foodFrame';
-        foodFrame.appendChild(foodFrameTem);
-    });  
+    // condition statement ??
+    if (Object.keys(foodName.meals).length > 0) {
+        const foodName1 = foodName.meals;
+        let foodFrame = document.getElementById('foodItem');
+        let foodIngredient = document.getElementById('foodIngredient');
+        foodFrame.innerHTML = '';
+        foodName1.forEach(eachFood => {
+            const name = eachFood.strMeal;
+            const image = eachFood.strMealThumb;
+            const foodFrameTem = document.createElement('div');
+            foodFrameTem.innerHTML = '';
+            const info = ` <div onclick= "singleItem('${name}')" >
+            <img  src='${image}' style='width: 250px;'>
+            <h5> ${name}</h5>
+            </div>
+            `;
+            foodFrameTem.innerHTML = info;
+            foodIngredient.innerHTML ='';
+            foodFrameTem.className = 'foodFrame';
+            foodFrame.appendChild(foodFrameTem);
+        });
+    } 
+
+  
 }
 
 const singleItem = parameter => {
-    // console.log(parameter);
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${parameter}`
     fetch(url)
     .then(res => res.json())
@@ -41,15 +44,7 @@ const Instruction = item => {
     const name = item.strMeal;
     const img = item.strMealThumb;
     const foodIngredient = document.getElementById('foodIngredient');
-    foodIngredient.className = 'foodIngredient'
-    for (let i = 1; i < 12; i++) {
-        // const array = [];
-        // array[i] = item[`strIngredient${i}`];   // item.strIngredient1, item.strIngredient2,item.strIngredient3 ...
-        const array=[];
-        const ingredient = 'strIngredient'+i;
-        array[i] = item[ingredient ];  
-        console.log(array[i]); 
-    }
+    foodIngredient.className = 'foodIngredient';
     
     const info1 = `
         <div id='ingredientDiv'>
